6980c03b452d7dcbe8e201de1e483ea4649550b5
ankush adhikari <ankushadhikari321@gmail.com>
2025-09-04 23:53:36 +0530

    deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Bot from './components/bot'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className='text-red-500'>Ankush</div>
-    </>
+<Bot/>
+ </>
   )
 }
 
